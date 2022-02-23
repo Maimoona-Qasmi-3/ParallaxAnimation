d497d8cc0d4d2b95442b95512ef8a57e662233ef
Maimoona <maimoonaqasmi3@gmail.com>
2022-02-23 20:32:54 +0500

    23-feb-22

---
+++ java.js
@@ -0,0 +1,12 @@
+
+var div1 = $("div.img1");
+var img1 = $("img.STOP");
+var img2 = $("img.FIND");
+$(body).click(function() {
+  if(img1.hasClass("image1")){
+    img1.removeClass("image1");
+
+  }else {
+    alert("hjhf");
+  }
+})
